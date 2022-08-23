23d9a738cdba0ca26ca25215d9d7d99c763034c6
chhun93 <chhun3830@gmail.com>
2022-08-23 18:14:46 +0900

    JSOUP 활용한 파싱(우체국 택배 정보)

--- src/main/java/com/postservice/chhun/dto/DeliveryInfo.java
+++ src/main/java/com/postservice/chhun/dto/DeliveryInfo.java
@@ -1,22 +1,50 @@
 package com.postservice.chhun.dto;
 
+import java.util.regex.Pattern;
+
 public class DeliveryInfo {
     private String who;
     private String where;
+    private String whenDate;
+    private String whenTime;
+    private String status;
 
-    public String getWho() {
-        return who;
-    }
-
-    public void setWho(String who) {
-        this.who = who;
-    }
-
-    public String getWhere() {
-        return where;
+    public boolean setInfo(String text) {
+        boolean ret = false;
+        if (ret = Pattern.matches("^\\d{4}.(0[1-9]|1[012]).(0[1-9]|[12][0-9]|3[01])$", text)) {
+            setWhenDate(text);
+        } else if (ret = Pattern.matches("^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", text)) {
+            setWhenDate(text);
+        } else if (ret = Pattern.matches("^\\d{2}:\\d{2}:\\d{2}$", text)) {
+            setWhenTime(text);
+        } else if (ret = Pattern.matches("^\\d{2}:\\d{2}$", text)) {
+            setWhenTime(text);
+        }
+        return ret;
     }
 
     public void setWhere(String where) {
         this.where = where;
+    }
+    public void setWhenDate(String whenDate) {
+        this.whenDate = whenDate;
+    }
+
+    public void setWhenTime(String whenTime) {
+        this.whenTime = whenTime;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryInfo{" +
+                "where='" + where + '\'' +
+                ", whenDate='" + whenDate + '\'' +
+                ", whenTime='" + whenTime + '\'' +
+                ", status='" + status + '\'' +
+                '}';
     }
 }
--- src/main/java/com/postservice/chhun/service/SearchPost.java
+++ src/main/java/com/postservice/chhun/service/SearchPost.java
@@ -2,19 +2,66 @@ package com.postservice.chhun.service;
 
 import com.postservice.chhun.dto.DeliveryInfo;
 import com.postservice.chhun.dto.ResponseVO;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class SearchPost {
 
-    public ResponseVO searchPost(String postNumber){
+    public ResponseVO searchPost(String postNumber) {
         ResponseVO responseVO = new ResponseVO("fail");
-        DeliveryInfo deliveryInfo = new DeliveryInfo();
-        deliveryInfo.setWho("chhun");
-        deliveryInfo.setWhere("화양동");
-        responseVO.setStatus("success");
-        responseVO.setResult(deliveryInfo);
-
+        searchPost_kpost(postNumber);
         return responseVO;
+    }
+
+    private List<DeliveryInfo> searchPost_kpost(String postNumber) {
+        String url = "https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm";
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+                .fromUriString(url)
+                .queryParam("sid1", postNumber);
+
+        Connection conn = Jsoup.connect(uriComponentsBuilder.toUriString());
+
+        try {
+            ArrayList<DeliveryInfo> deliveryInfos = new ArrayList<>();
+            Document document = conn.get();
+            Elements processTableEl = document.select("#processTable").select("tbody");
+
+            for (Element elementTableRow : processTableEl.select("tr")) {
+                DeliveryInfo deliveryInfo = new DeliveryInfo();
+                for (Element elementTableColumn : elementTableRow.select("td")) {
+                    if (deliveryInfo.setInfo(elementTableColumn.text()))
+                        continue;
+
+                    if (elementTableColumn.select("span").hasClass("evtnm")) {
+                        deliveryInfo.setStatus(elementTableColumn.select("span.evtnm").text());
+                    }
+                    else if(elementTableColumn.select("a span").size()>0){
+                        deliveryInfo.setWhere(elementTableColumn.select("a span").text());
+                    }
+                    else{
+                        deliveryInfo.setWhere(elementTableColumn.select("a").text());
+                    }
+                }
+                deliveryInfos.add(deliveryInfo);
+            }
+
+            for(DeliveryInfo deliveryInfo : deliveryInfos){
+                System.out.println("deliveryInfo = " + deliveryInfo.toString());
+            }
+
+            return deliveryInfos;
+        } catch (IOException ignored) {
+        }
+
+        return null;
     }
 }
